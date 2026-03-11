d38bb6ca15c26640bca11a028f0e90b6506c4ea8
fazlie <fazlie14@gmail.com>
2026-03-11 22:42:28 +0800

    add modal and handle form

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+const modal = document.querySelector("#modal")
+const openBtn = document.querySelector("#openModal")
+const closeBtn = document.querySelector("#closeModal")
+const bookForm = document.querySelector('#bookForm')
 
 const myLibrary = []
 
@@ -11,6 +14,21 @@ function Book(title,author,pages,read){
 
 }
 
+bookForm.addEventListener('submit',function(e){
+  e.preventDefault()
+
+  const title = document.querySelector('#title').value
+  const author = document.querySelector('#author').value
+  const pages = document.querySelector('#pages').value
+  const read = document.querySelector('#read').checked
+
+  addBookToLibrary(title, author, pages, read)
+  bookForm.reset()
+
+  modal.classList.add('hidden')
+
+})
+
 function addBookToLibrary ( title, author, pages, read){
   const book = new Book(title, author, pages, read)
   myLibrary.push(book)
@@ -22,32 +40,54 @@ function displayBooks(){
   const bookCard = document.querySelector('#bookCard')
   const books = myLibrary.map(book =>`
 
-    <div class='card'>
-           <h3> ${book.title}</h3>
-          <p>${book.author}</p>
-           <span>${book.read? 'read': 'not read yet'}</span>
+    <div class="bg-white-300 shadow-lg rounded-xl p-2 w-full">
+
+          <h3 class="text-xl text-center font-bold text-gray-800"> ${book.title}</h3>
+          <p class="text-gray-600 mt-1">${book.author}</p>
+          <p class="text-sm text-gray-500 mt-1">${book.pages}</p>
+          <span class="inline-block mt-3 px-3  text-sm bg-green-100 text-green-700 rounded-full">${book.read? 'read': 'not read yet'}</span>
+       
+
           
     </div>`
 
 
   )
-
-  // for(const books of myLibrary){
-    
-  //     book.push( `<div>
-  //       <h1> ${books.title}</h1>
-  //      <p>${books.author}</p>
-  //       <span>${books.read? 'read': 'not read yet'}</span>
-        
-  //      </div>`)
-     
-  // }
-
   return  bookCard.innerHTML = books.join('')
    
 }
 
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+ addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
+  addBookToLibrary('the hobbit','fazlie',204,false)
 
-addBookToLibrary('The Hobbit','Jrr',12,true)
-addBookToLibrary('The Hobbit','Jrr',12,false)
-console.log(myLibrary)
+
+openBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden")
+})
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.add("hidden")
+})
+
+
